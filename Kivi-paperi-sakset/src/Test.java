1697e11f473c194e6ef442f07e10aef48995b0c0
jjuh00 <jamsen.juho@gmail.com>
2025-02-04 13:32:15 +0200

    Update settings.json

--- Kivi-paperi-sakset/src/Test.java
+++ Kivi-paperi-sakset/src/Test.java
@@ -1,17 +1,27 @@
-/*
+
 package src;
 
 // Halutessasi (eli jos haluat ajaan tämän testin) ota tämä pois kokonaan kommenteista
 // HUOM! Katso .vscode/settings.json -tiedosto ensiksi
 
 import org.junit.jupiter.api.*;
-import java.util.*;
-import java.io.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Test {
-    
-    Toteutus tälle testille tulee pian
+import java.beans.Transient;
 
+public class Test {
+    private Pelaaminen peli;
+    private Liikkeet liikkeet;
+
+    @BeforeEach
+    void setUp() {
+        peli = new Pelaaminen();
+        liikkeet = new Liikkeet();
+    }
+
+    @Test
+    void haeTietokoneenLiike_helppo() {
+        
+    }
 }
-*/
+
